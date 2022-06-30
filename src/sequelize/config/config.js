b1f743042a8ffe8d423c475c2ccdcf3c37a69372
paolofullone <paolo.enrico@gmail.com>
2022-06-30 08:33:54 -0300

    config.js

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -15,7 +15,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
